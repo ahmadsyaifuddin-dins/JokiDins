47c433421abd262a33328dc7d35c5bc5c49c1890
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-02-17 16:10:59 +0800

    REACT JS ⚛️+ VITE ⚡ + UI TAILWIND 🍃

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
+      Tailwind Berhasil Dipasang! 🚀
+    </div>
+  )
+}
+
+export default App
