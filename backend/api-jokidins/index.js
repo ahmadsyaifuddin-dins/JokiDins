94ad801c16289bd44be142fcaa3a5182e72bcc8d
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-02-18 18:04:48 +0800

    mock data

--- backend/api-jokidins/index.js
+++ backend/api-jokidins/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "API JokiDins Online 🚀" });
+// Mock Data untuk tugas joki
+const tasks = [
+  { id: 1, task_name: "Pembuatan Website", status: "Pending" },
+  { id: 2, task_name: "Pembuatan Aplikasi Mobile", status: "Progress" },
+  { id: 3, task_name: "Bantu Tugas Kuliah", status: "Selesai" },
+];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
 
 module.exports = app;
+
