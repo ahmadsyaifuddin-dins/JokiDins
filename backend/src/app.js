93d573e1a8fc704297f3a3144d496c551cb393af
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-02-20 00:36:31 +0800

    setup backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('API Running...');
+  });
+  
 // Koneksi ke MongoDB
 connectDB();
 
@@ -20,3 +24,5 @@ app.use("/api", routes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
+
+
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log("MongoDB Connected ✅");
+    console.log(`MongoDB Connected ${conn.connection.host} ✅`);
   } catch (err) {
     console.error("MongoDB Connection Error ❌", err);
     process.exit(1);
