a16545b2e211e86df7cc4b53ec19d7bb51983ff1
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-03-04 15:57:47 +0800

    delete config

--- backend/src/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const isDevelopment = window.location.hostname === 'localhost';
-
-const API_URL = isDevelopment
-  ? 'https://jokidins-production.up.railway.app'
-  : 'https://jokidins-production.up.railway.app';
-
-export default API_URL;
