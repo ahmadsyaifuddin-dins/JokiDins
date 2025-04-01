c723b2e65c29a00b00116bb87eace65cd3f26db9
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-04-01 14:17:05 +0800

    online

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://jokidins-production.up.railway.app";
 console.log("Check ENV: ", import.meta.env.VITE_API_URL);
