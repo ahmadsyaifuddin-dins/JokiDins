1b82778602e42200b74889a2743664d6ee898242
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-04-03 12:26:07 +0800

    Update Activity.js

--- backend/src/models/Activity.js
+++ backend/src/models/Activity.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 
 const ActivitySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  order: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
   timestamp: { type: Date, default: Date.now },
-  type: { type: String, required: true },
   description: { type: String, required: true }
 });
 
